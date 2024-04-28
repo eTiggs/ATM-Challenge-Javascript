3ee93c629a3bf4c0cc06448980908ecd5ef769ba
eTiggs <edunwo01@student.bbk.ac.uk>
2024-04-28 14:20:43 +0100

    Statement 1 Test 3 Passed

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -12,7 +12,7 @@ describe("Statement 1 Tests:", () => {
         actual = undefined;
     });
 
-    it("should set a balance to the bank account object", () => {
+    it("should instantiate a bank account object", () => {
         // Arrange
         // Act
         testAccount = new account();
@@ -23,13 +23,24 @@ describe("Statement 1 Tests:", () => {
         expect(actual).toBe(expected);
     });
 
-    it("should instantiate a bank account object", () => {
+    it("should set a balance to the bank account object", () => {
         // Arrange
         // Act
         testAccount = new account(1000);
         // Assert
         expected = true;
         actual = testAccount.balance === 1000;
+
+        expect(actual).toBe(expected);
+    });
+
+    it("should return an error message if the added balance is negative", () => {
+        // Arrange
+        // Act
+        testAccount = new account(-1000);
+        // Assert
+        expected = true;
+        actual = testAccount.balance === 0;
 
         expect(actual).toBe(expected);
     });
--- src/account.js
+++ src/account.js
@@ -2,6 +2,10 @@ export default class account {
     balance = 0;
 
     constructor(initialBalance) {
+        if (initialBalance < 0) {
+            return "Initial balance cannot be negative";
+        }
+
         this.balance = initialBalance;
     }
 }
