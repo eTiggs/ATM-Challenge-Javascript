599bb888d4dd1380b559d7e0e17c15676ce30e18
eTiggs <edunwo01@student.bbk.ac.uk>
2024-04-28 14:06:55 +0100

    Statement 1 Test 2 Passed

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -7,18 +7,29 @@ describe("Statement 1 Tests:", () => {
     let actual;
 
     beforeEach(() => {
-        testAccount = new account();
+        testAccount = new account(0);
         expected = undefined;
         actual = undefined;
     });
 
-    it("should instantiate a bank account object", () => {
+    it("should set a balance to the bank account object", () => {
         // Arrange
         // Act
         testAccount = new account();
         // Assert
         expected = true;
         actual = testAccount instanceof account;
+
+        expect(actual).toBe(expected);
+    });
+
+    it("should instantiate a bank account object", () => {
+        // Arrange
+        // Act
+        testAccount = new account(1000);
+        // Assert
+        expected = true;
+        actual = testAccount.balance === 1000;
 
         expect(actual).toBe(expected);
     });
--- src/account.js
+++ src/account.js
@@ -1,2 +1,7 @@
 export default class account {
+    balance = 0;
+
+    constructor(initialBalance) {
+        this.balance = initialBalance;
+    }
 }
